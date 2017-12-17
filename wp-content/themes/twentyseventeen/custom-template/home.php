a1ab46d9b08b4faab10f09c1f454d2f261a290e7
uttam <webuttam91@gmail.com>
2017-12-17 15:22:49 +0530

    Sucasa file upload

--- wp-content/themes/twentyseventeen/custom-template/home.php
+++ wp-content/themes/twentyseventeen/custom-template/home.php
@@ -7,7 +7,7 @@ get_header(); ?>
 
 <div class="home-banner">
 	<div class="container">
-		
+		cbfngngmh  hgmymymu jmu6mu6 6um
 		lskhfieifheoifhioehfoehvihvihio
 	</div>
 </div><!-- .home-banner -->
