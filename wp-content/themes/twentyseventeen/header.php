d232dbbb6c156ba8b19320da2287308ce317e47b
uttam <webuttam91@gmail.com>
2017-12-17 14:19:40 +0530

    Sucasa file upload

--- wp-content/themes/twentyseventeen/header.php
+++ wp-content/themes/twentyseventeen/header.php
@@ -20,6 +20,7 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/bootstrap.css" />
 <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/main.css" />
+<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/animate.css" />
 <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
 <?php wp_head(); ?>
